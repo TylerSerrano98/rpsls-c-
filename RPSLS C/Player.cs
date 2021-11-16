bef30dd10b4a53d0862aa55191219c5fae6c2ca1
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-11-16 14:38:20 -0600

    created player class with list of gestures and some gesture choice functionality

---
+++ RPSLS C/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS_C
+{
+    class Player
+    {
+        public int wins;
+        public string choice;
+        public string[] gestures = new string[5];
+
+        public Player()
+        {
+            gestures[0] = "rock";
+            gestures[1] = "paper";
+            gestures[2] = "scissors";
+            gestures[3] = "lizard";
+            gestures[4] = "spock";
+        }
+
+        public virtual string Make_choice()
+        {
+            return "";
+        }
+
+        public virtual string Make_choice(int round, int opponent_wins)
+        {
+            return "";
+        }
+    }
+}
